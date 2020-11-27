06f99c4dd1455e46f52498d4a16fce39adda0b2d
Khalifa I9 <k.rmili@exchange-data.com>
2020-11-27 13:31:43 +0000

    Models defined with relationships

---
+++ database/migrations/2020_11_27_130531_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->float('price');
+            $table->string('image')->unique();
+            $table->timestamps();
+            $table->bigInteger('category_id')->unsigned()->index();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('products');
+        Schema::enableForeignKeyConstraints();
+
+    }
+}
